f254c281efbe239b58d9742fdc3728191dfdc807
Kevin Moore <moore.kevin.p@gmail.com>
2020-04-13 13:30:22 -0400

    modified test-helper and basic-auth to hash passwords

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,3 +1,6 @@
+'use strict';
+const bcrypt = require('bcrypptjs');
+
 function makeUsersArray() {
   return [
     {
@@ -6,7 +9,7 @@ function makeUsersArray() {
       full_name: 'Test user 1',
       nickname: 'TU1',
       password: 'password',
-      date_created: '2029-01-22T16:28:32.615Z',
+      date_created: '2029-01-22T16:28:32.615Z'
     },
     {
       id: 2,
@@ -14,7 +17,7 @@ function makeUsersArray() {
       full_name: 'Test user 2',
       nickname: 'TU2',
       password: 'password',
-      date_created: '2029-01-22T16:28:32.615Z',
+      date_created: '2029-01-22T16:28:32.615Z'
     },
     {
       id: 3,
@@ -22,7 +25,7 @@ function makeUsersArray() {
       full_name: 'Test user 3',
       nickname: 'TU3',
       password: 'password',
-      date_created: '2029-01-22T16:28:32.615Z',
+      date_created: '2029-01-22T16:28:32.615Z'
     },
     {
       id: 4,
@@ -30,9 +33,9 @@ function makeUsersArray() {
       full_name: 'Test user 4',
       nickname: 'TU4',
       password: 'password',
-      date_created: '2029-01-22T16:28:32.615Z',
+      date_created: '2029-01-22T16:28:32.615Z'
     },
-  ]
+  ];
 }
 
 function makeThingsArray(users) {
@@ -69,7 +72,7 @@ function makeThingsArray(users) {
       date_created: '2029-01-22T16:28:32.615Z',
       content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?',
     },
-  ]
+  ];
 }
 
 function makeReviewsArray(users, things) {
@@ -135,13 +138,13 @@ function makeReviewsArray(users, things) {
 
 function makeExpectedThing(users, thing, reviews=[]) {
   const user = users
-    .find(user => user.id === thing.user_id)
+    .find(user => user.id === thing.user_id);
 
   const thingReviews = reviews
-    .filter(review => review.thing_id === thing.id)
+    .filter(review => review.thing_id === thing.id);
 
-  const number_of_reviews = thingReviews.length
-  const average_review_rating = calculateAverageReviewRating(thingReviews)
+  const number_of_reviews = thingReviews.length;
+  const average_review_rating = calculateAverageReviewRating(thingReviews);
 
   return {
     id: thing.id,
@@ -158,25 +161,25 @@ function makeExpectedThing(users, thing, reviews=[]) {
       nickname: user.nickname,
       date_created: user.date_created,
     },
-  }
+  };
 }
 
 function calculateAverageReviewRating(reviews) {
-  if(!reviews.length) return 0
+  if(!reviews.length) return 0;
 
   const sum = reviews
     .map(review => review.rating)
-    .reduce((a, b) => a + b)
+    .reduce((a, b) => a + b);
 
-  return Math.round(sum / reviews.length)
+  return Math.round(sum / reviews.length);
 }
 
 function makeExpectedThingReviews(users, thingId, reviews) {
   const expectedReviews = reviews
-    .filter(review => review.thing_id === thingId)
+    .filter(review => review.thing_id === thingId);
 
   return expectedReviews.map(review => {
-    const reviewUser = users.find(user => user.id === review.user_id)
+    const reviewUser = users.find(user => user.id === review.user_id);
     return {
       id: review.id,
       text: review.text,
@@ -189,8 +192,8 @@ function makeExpectedThingReviews(users, thingId, reviews) {
         nickname: reviewUser.nickname,
         date_created: reviewUser.date_created,
       }
-    }
-  })
+    };
+  });
 }
 
 function makeMaliciousThing(user) {
@@ -200,24 +203,24 @@ function makeMaliciousThing(user) {
     date_created: new Date().toISOString(),
     title: 'Naughty naughty very naughty <script>alert("xss");</script>',
     user_id: user.id,
-    content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
-  }
+    content: 'Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.',
+  };
   const expectedThing = {
     ...makeExpectedThing([user], maliciousThing),
     title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
-    content: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
-  }
+    content: 'Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.',
+  };
   return {
     maliciousThing,
     expectedThing,
-  }
+  };
 }
 
 function makeThingsFixtures() {
-  const testUsers = makeUsersArray()
-  const testThings = makeThingsArray(testUsers)
-  const testReviews = makeReviewsArray(testUsers, testThings)
-  return { testUsers, testThings, testReviews }
+  const testUsers = makeUsersArray();
+  const testThings = makeThingsArray(testUsers);
+  const testReviews = makeReviewsArray(testUsers, testThings);
+  return { testUsers, testThings, testReviews };
 }
 
 function cleanTables(db) {
@@ -227,9 +230,25 @@ function cleanTables(db) {
       thingful_users,
       thingful_reviews
       RESTART IDENTITY CASCADE`
-  )
+  );
 }
 
+function seedUsers(db, users) {
+  const preppedUsers = users.map(user => ( {
+    ...user,
+    password: bcrypt.hashSync(user.password, 1)
+  }));
+  return db.into('thingful_users').insert(preppedUsers)
+    .then(() => 
+    //update the auto sequence to stay in sync
+      db.raw(
+        'SELECT setval (\'thingful_users_id_seq\', ?)',
+        [users[users.length -1].id]
+      )
+    );
+}
+
+/*
 function seedThingsTables(db, users, things, reviews=[]) {
   return db
     .into('thingful_users')
@@ -241,18 +260,35 @@ function seedThingsTables(db, users, things, reviews=[]) {
     )
     .then(() =>
       reviews.length && db.into('thingful_reviews').insert(reviews)
+    );
+}
+*/
+
+function seedThingsTables(db, users, things, reviews=[]) {
+  return db.transaction(async trx => {
+    await seedUsers(trx, users)
+    await trx.into('thingful_things').insert(things)
+    await trx.raw(
+      `SELECT setval('thingful_things_id_seq', ?)`,
+      [things[things.length -1].id],
     )
+    if (reviews.length) {
+      await trx.into('thingful_reviews').insert(things)
+      await trx.raw(
+        `SELECT setval('thingful_reviews_id_seq', ?)`,
+        [reviews[reviews.length -1].id],
+      )
+    }
+  })
 }
 
 function seedMaliciousThing(db, user, thing) {
-  return db
-    .into('thingful_users')
-    .insert([user])
+  return seedUsers(db, [user])
     .then(() =>
       db
         .into('thingful_things')
         .insert([thing])
-    )
+    );
 }
 
 function makeAuthHeader(user) {
@@ -273,4 +309,5 @@ module.exports = {
   seedThingsTables,
   seedMaliciousThing,
   makeAuthHeader,
-}
+  seedUsers,
+};
